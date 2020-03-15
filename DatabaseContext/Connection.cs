38b0cf753f599a7ce4b9a3a7b08f1b49465c51d5
Ardin Biganiro <ardinbig@gmail.com>
2020-03-15 20:39:11 +0200

    Add connexion class

---
+++ DatabaseContext/Connection.cs
@@ -0,0 +1,59 @@
+﻿using MySql.Data.MySqlClient;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DatabaseContext
+{
+    public class Connection : IConnection
+    {
+        private static Connection _con;
+
+        public static Connection Instance
+        {
+            get
+            {
+                if (_con == null)
+                    _con = new Connection();
+                return _con;
+            }
+        }
+
+        /// <summary>
+        /// Connexion à la base de données
+        /// </summary>
+        public IDbConnection Get { get; set; }
+
+        /// <summary>
+        /// Type de base de données sur laquelle se connecter
+        /// </summary>
+        public DatabaseType DbType { get; set; }
+
+        public IDbConnection Initialize(Database db)
+        {
+            string connectionString = "";
+            switch (DbType)
+            {
+                case DatabaseType.MySql:
+                    connectionString = $"Server={db.Server};Database={db.DbName};" +
+                        $"User id={db.User};Pwd={db.Password};Port={db.Port}";
+                    Get = new MySqlConnection(connectionString);
+                    break;
+                case DatabaseType.SqlServer:
+                    connectionString = $"Data Source={db.Server};Initial Catalog={db.DbName};" +
+                        $"User ID={db.User};Password={db.Password}";
+                    Get = new SqlConnection(connectionString);
+                    break;
+                case DatabaseType.PostgreSql:
+                    break;
+                case DatabaseType.Oracle:
+                    break;
+                default:
+                    connectionString = $"Server={db.Server};Database={db.DbName};" +
+                        $"User id={db.User};Pwd={db.Password}";
+                    Get = new MySqlConnection(connectionString);
+                    break;
+            }
+            return Get;
+        }
+    }
+}
---
+++ DatabaseContext/IConnection.cs
@@ -0,0 +1,14 @@
+﻿using System.Data;
+
+namespace DatabaseContext
+{
+    public interface IConnection
+    {
+        /// <summary>
+        /// Retourne l'instance de la connexion à la base de données
+        /// </summary>
+        /// <param name="db"></param>
+        /// <returns></returns>
+        IDbConnection Initialize(Database db);
+    }
+}
