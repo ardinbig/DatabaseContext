7bc05f534ce452e02ac20319086c525ea75efb08
Ardin Biganiro <ardinbig@gmail.com>
2020-03-15 19:57:51 +0200

    Add database class

---
+++ DatabaseContext/Database.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseContext
+{
+    public class Database
+    {
+        public Database() { }
+
+        private string _server = "localhost";
+        private string _dbName = "";
+        private string _user = "";
+        private string _password = "";
+        private int _port = 0;
+
+        public string Server
+        {
+            get
+            {
+                return _server;
+            }
+
+            set
+            {
+                _server = value;
+            }
+        }
+
+        public string DbName
+        {
+            get
+            {
+                return _dbName;
+            }
+
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new Exception("No null database");
+                else
+                    _dbName = value;
+            }
+        }
+
+        public string User
+        {
+            get
+            {
+                return _user;
+            }
+
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new Exception("No null user");
+                else
+                    _user = value;
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return _password;
+            }
+
+            set
+            {
+                _password = value;
+            }
+        }
+
+        public int Port
+        {
+            get
+            {
+                return _port;
+            }
+
+            set
+            {
+                _port = value;
+            }
+        }
+    }
+}
---
+++ DatabaseContext/DatabaseType.cs
@@ -0,0 +1,13 @@
+﻿namespace DatabaseContext
+{
+    /// <summary>
+    /// Enumération permettant de liste les types de base des données
+    /// </summary>
+    public enum DatabaseType
+    {       
+        MySql,
+        SqlServer,
+        PostgreSql,
+        Oracle
+    }
+}
